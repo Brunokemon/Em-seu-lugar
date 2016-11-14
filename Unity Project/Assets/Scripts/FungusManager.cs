395696c5290322a246351491804d5e69eb955f85
Brunokemon <bkviha@gmail.com>
2016-11-14 07:15:37 -0200

    Refatorado código

--- Unity Project/Assets/Scripts/FungusManager.cs
+++ Unity Project/Assets/Scripts/FungusManager.cs
@@ -19,77 +19,72 @@ public class FungusManager : MonoBehaviour
 	private string blockName;
 	private int commandID;
 
-	float messageTimer = 0f;
+	public float messageTimer = 0f;
 
 	public GameObject isTypingIcon;
 
 	//Lista para dizer quantas mensagens ainda estão sendo enviadas
-	List<float> runningCorroutines = new List<float> ();
+	private List<float> runningCorroutines = new List<float> ();
 
 	void Start ()
 	{
 		viewManager = gameObject.GetComponent<ViewManager> ();
 		textReader = gameObject.GetComponent<TextReader> ();
-
-		//Este comando pega o nome do bloco em execucao
-		blockName = currentFlowchart.GetExecutingBlocks () [0].blockName;
-
-		//Encontra o block
-		currentBlock = currentFlowchart.FindBlock (blockName);
-		commandID = currentBlock.commandList [0].itemId;
-
-		//SaveGame.SaveOrder (SaveGame.JuliaExecutedCommands, commandID);
 	}
 
 	void Update ()
 	{
-		this.ActiveFlowchart ();		
-		//Check if block has changed update variables
-		if (currentFlowchart.GetExecutingBlocks () != null) {
-			if (blockName != currentFlowchart.GetExecutingBlocks () [0].blockName) {
+
+		//if there is a flowchart active
+		if (this.ActiveFlowchart ()) {	
+
+			//if has changed block
+			if (currentFlowchart.GetExecutingBlocks () != null && currentFlowchart.GetExecutingBlocks ().Count != 0 && blockName != currentFlowchart.GetExecutingBlocks () [0].blockName) {
+
 				blockName = currentFlowchart.GetExecutingBlocks () [0].blockName;
 				currentBlock = currentFlowchart.FindBlock (blockName);
+
 				messageTimer = 0f;
 			}
-		}
 
-		//if commandID has changed printSayMessage from last command and update variables
-		if (currentBlock.activeCommand != null && commandID != currentBlock.activeCommand.itemId) {
+			//if commandID has changed printSayMessage from last command and update
+			if (currentBlock.activeCommand != null && commandID != currentBlock.activeCommand.itemId) {
 
-			//Mostra o comando anterior para dar tempo de o SayDialog do Player digitar antes de mostrar a mensagem no Histórico do Chat
-			if (textReader.dialogsJulia.ContainsKey (commandID)) {
-				string phrase = textReader.FindPhrase (currentFlowchart, commandID);
-				messageTimer += phrase.Length * 0.08f;
-				StartCoroutine (CallSayMessageWithDelay (phrase, textReader.dialogsJulia [commandID].character, messageTimer));
+				//Mostra o comando anterior para dar tempo de o SayDialog do Player digitar antes de mostrar a mensagem no Histórico do Chat
+				if (textReader.dialogsJulia.ContainsKey (commandID)) {
+					string phrase = textReader.FindPhrase (currentFlowchart, commandID);
+					messageTimer += phrase.Length * 0.08f;
+					StartCoroutine (CallSayMessageWithDelay (phrase, textReader.dialogsJulia [commandID].character, messageTimer));
+				}
+
+				//updates to actual commandID
+				commandID = currentBlock.activeCommand.itemId;
+
+				/* Para ser usado futuramente
+				//Pega o comando atual
+				Command command = currentBlock.activeCommand;
+				//Verifica se é um Say
+				if (command.GetType ().Name == "Say") {
+					Say commandSay = command as Say;
+					//string phrase = textReader.FindPhrase (JuliaFlowchart, commandID);
+					//viewManager.PrintMessage (phrase, (Say)say.character.name);
+				}
+
+				//Verifica se é um Wait
+				if (command.GetType ().Name == "Wait") {
+					Wait commandWait = command as Wait;
+					//duracao do Wait Command
+					float duration = commandWait._duration;
+				}
+
+				SaveGame.SaveOrder (currentFlowchart.name, commandID);
+				*/
 			}
-
-			commandID = currentBlock.activeCommand.itemId;
-
-			/*
-
-			//Pega o comando atual
-			Command command = currentBlock.activeCommand;
-			//Verifica se é um Say
-			if (command.GetType ().Name == "Say") {
-				Say commandSay = command as Say;
-				//string phrase = textReader.FindPhrase (JuliaFlowchart, commandID);
-				//viewManager.PrintMessage (phrase, (Say)say.character.name);
-			}
-
-			//Verifica se é um Wait
-			if (command.GetType ().Name == "Wait") {
-				Wait commandWait = command as Wait;
-				//duracao do Wait Command
-				float duration = commandWait._duration;
-			}
-
-			SaveGame.SaveOrder (currentFlowchart.name, commandID);
-
-			*/
 		}
 	}
 
-	IEnumerator CallSayMessageWithDelay (string text, string character, float delay)
+	//obs: Tem de ser passado pro VIewManager.cs
+	private IEnumerator CallSayMessageWithDelay (string text, string character, float delay)
 	{
 		//o Character está vindo com um character "invisivel" mais. Devemos remover a ultima letra para garantir que os nomes estejam corretos
 		string correctName = character.Substring (0, character.Length - 1);
@@ -109,13 +104,20 @@ public class FungusManager : MonoBehaviour
 		runningCorroutines.Remove (delay);
 	}
 
-	void ActiveFlowchart ()
+	//Checks if there is a Flowchart active, and sets the variable currentFlowchart to the active Flowchart
+	private bool ActiveFlowchart ()
 	{
 		//Modelo para ser usado em todos os flowcharts
-		//if (OtherNPCFlowchart.isActiveAndEnabled)
+		//if (OtherNPCFlowchart.isActiveAndEnabled){
 		//	currentFlowchart = OtherNPCFlowchart;
+		//	return true;
+		//}
 
-		if (JuliaFlowchart.isActiveAndEnabled)
+		if (JuliaFlowchart.isActiveAndEnabled) {
 			currentFlowchart = JuliaFlowchart;
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
