526c7656dc8dd41bc5dd145fbf67ccebfb5c0ac1
Brunokemon <bkviha@gmail.com>
2016-11-21 10:56:47 -0200

    Criadas funções de Save e Load

--- Unity Project/Assets/Scripts/DictionaryClasses.cs
+++ Unity Project/Assets/Scripts/DictionaryClasses.cs
@@ -1,19 +1,20 @@
 ﻿//Script usado para guardar as classes criadas para guardar valores
 using System.Collections;
 
+[System.Serializable]
 public class DictionaryClasses
 {
 	//Classe para usar com Dictionary para guardar valores (Block e commandID) de cada SayDialog no FungusManager.cs
+	[System.Serializable]
 	public class SaveValues
 	{
-		public string flowchart { get; set; }
-
-		public string block { get; set; }
-
 		public int command { get; set; }
+
+		public float time { get; set; }
 	}
 
 	//Classe para usar com Dictionary para guardar valores (frase e character) de cada SayDialog em TextReader.cs
+	[System.Serializable]
 	public class SayValues
 	{
 		public string text { get; set; }
--- Unity Project/Assets/Scripts/SaveGame.cs
+++ Unity Project/Assets/Scripts/SaveGame.cs
@@ -4,19 +4,49 @@ using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-//Proxima versao mudar nome para SaveLoad
-//Duas opcoes possiveis para classe:
-//1-deixar a classe static e tira heranca do MonoBehaviour e chama a funcao de salvar em cada operacao que considerar que deve salvar
-//2-deixar como herdeira da classe MonoBehaviour e instancia em um objeto para com as funcoes Start e Update fazer automaticamente o Load e o Save
-public class SaveGame : MonoBehaviour
+//Obs: Proxima versao mudar nome para SaveLoad
+[System.Serializable]
+public static class SaveGame
 {
-	//Modelo para todos os flowcharts diferentes
-	//public static List<DictionaryClasses.SaveValues> NPCExecutedCommands = new List<DictionaryClasses.SaveValues> ();
 
-	public static List<DictionaryClasses.SaveValues> JuliaExecutedCommands = new List<DictionaryClasses.SaveValues> ();
+	//Classe onde ficarao reunidas todas as informacoes a serem salvas
+	[System.Serializable]
+	public class SaveFile
+	{
+		//Modelo para todos os flowcharts diferentes
+		//public static List<DictionaryClasses.SaveValues> NPCExecutedCommands = new List<DictionaryClasses.SaveValues> ();
 
-	//Salva ordem e ID's dos comandos executados
-	public static void SaveOrder (string flowchartName, int command)
+		public List<DictionaryClasses.SaveValues> JuliaExecutedCommands = new List<DictionaryClasses.SaveValues> ();
+	}
+
+	//Instancia da classe onde serao colocadas todas as informacoes a serem salvas
+	public static SaveFile saveFile = new SaveFile ();
+
+	//variavel para indicar se ja terminou o loading
+	public static bool loaded = false;
+
+	//Obs: Proxima versao mudar nome para SaveGame
+	public static void Save ()
+	{
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/save.esl");
+		bf.Serialize (file, saveFile);
+		file.Close ();
+	}
+
+	//Obs: Proxima versao mudar nome para LoadGame
+	public static void Load ()
+	{
+		if (File.Exists (Application.persistentDataPath + "/save.esl")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/save.esl", FileMode.Open);
+			saveFile = (SaveFile)bf.Deserialize (file);
+			file.Close ();
+		}
+	}
+
+	//Atualiza lista de ID's dos comandos executados do flowchart especificado
+	public static void UpdateExecutedCommands (string flowchartName, int command)
 	{
 		DictionaryClasses.SaveValues save = new DictionaryClasses.SaveValues ();
 		save.command = command;
@@ -27,21 +57,10 @@ public class SaveGame : MonoBehaviour
 	private static List<DictionaryClasses.SaveValues> FlowchartSaveList (string flowchartName)
 	{
 		switch (flowchartName) {
-		case "JuliaFlowchart":
-			return JuliaExecutedCommands;
+		case "Julia":
+			return saveFile.JuliaExecutedCommands;
 		default:
 			return null;
 		}
-	}
-
-	void Start ()
-	{
-		//Funcao que verifica se existe um arquivo de save no local onde esta sendo rodado o jogo
-		//Funcao que faz load de Save caso exista
-	}
-
-	void Update ()
-	{
-		//Funcao que faz automaticamente o save
 	}
 }
