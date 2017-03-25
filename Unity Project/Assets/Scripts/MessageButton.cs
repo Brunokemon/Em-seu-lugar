7a9242d70fabcbd2306ede92e840ed8edc319fd9
thia9uera <thia9uera@gmail.com>
2017-03-25 17:21:33 +0000

    CHat layout

---
+++ Unity Project/Assets/Scripts/MessageButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageButton : MonoBehaviour
+{
+    [HideInInspector]
+    public string m_Message;
+
+    private Text msgText;
+
+    private void Awake()
+    {
+       msgText = transform.GetComponentInChildren<Text>();
+    }
+
+    public void Populate(string p_message)
+    {
+        msgText.text = m_Message = p_message;
+    }
+
+    public void ClickHandler()
+    {
+        if (m_Message == "") m_Message = msgText.text;
+        ChatController.Instance.FillInputField(m_Message);
+    }
+}
---
+++ Unity Project/Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TypewriterEffect : MonoBehaviour 
+{
+	private float m_Pause;
+
+	[SerializeField]
+	private int m_LettersPerSecond;
+	
+	private string m_FullText;
+
+	private Text m_Label;
+
+    private void Awake()
+    {
+        m_Label = gameObject.GetComponent<Text>();
+    }
+
+	public void StartEffect()
+	{
+        StopAllCoroutines();
+
+		m_FullText = m_Label.text;
+		
+		m_Label.text = "";
+		m_Pause = 1f / m_LettersPerSecond;
+
+		StartCoroutine(TypeText ());
+
+	}
+
+    public void StopEffect()
+    {
+        StopAllCoroutines();
+    }
+	
+	IEnumerator TypeText ()
+	{
+		foreach (char letter in m_FullText.ToCharArray()) 
+		{
+			gameObject.GetComponent<Text>().text += letter;
+
+			yield return new WaitForSeconds (m_Pause);
+		}      
+	}
+}
