0ad10bc0a0f841a0c425dff4ac6807df34adbc75
Brunokemon <bkviha@gmail.com>
2017-03-29 22:37:01 -0300

    Repassado pra este branch o chat que o Thiago fez de teste

---
+++ Unity Project/Assets/Scripts/BubbleView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleView : MonoBehaviour
+{
+
+    public void SetText(string p_message)
+    {
+        Text messageText = transform.GetComponentInChildren<Text>();
+
+        messageText.text = p_message;
+    }
+}
---
+++ Unity Project/Assets/Scripts/ChatController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatController : MonoBehaviour
+{
+    private static ChatController _instance;
+    public static ChatController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<ChatController>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public Transform m_Container;
+
+    public GameObject m_BubbleLeft;
+
+    public GameObject m_BubbleRight;
+
+    public Text m_InputText;
+
+    private TypewriterEffect m_textEffect;
+
+    [SerializeField]
+    private VerticalLayoutGroup layoutGroup;
+
+    private string message;
+
+    private void Awake()
+    {
+        m_textEffect = m_InputText.transform.GetComponent<TypewriterEffect>();
+    }
+
+    public void FillInputField(string p_message)
+    {
+        message = p_message;
+        m_InputText.text = p_message;
+        m_textEffect.StartEffect();
+    }
+
+    public void AddBubbleLeft(string p_message = "")
+    {
+        GameObject newMessage = Instantiate(m_BubbleLeft) as GameObject;
+
+        newMessage.transform.SetParent(m_Container, false);
+        newMessage.transform.GetComponent<BubbleView>().SetText(message);
+
+        ClearInput();
+    }
+
+    public void AddBubbleRight(string p_message = "")
+    {
+        m_textEffect.StopEffect();
+
+        GameObject newMessage = Instantiate(m_BubbleRight) as GameObject;
+
+        newMessage.transform.SetParent(m_Container, false);
+        newMessage.transform.GetComponent<BubbleView>().SetText(message);
+
+        ClearInput();
+    }
+
+    private void ClearInput()
+    {
+        m_InputText.text = ".";
+        m_InputText.text = "";
+        layoutGroup.CalculateLayoutInputVertical();
+    }
+}
