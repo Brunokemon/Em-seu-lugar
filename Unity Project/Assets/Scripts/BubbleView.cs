18b20f1bd0ecfddc55cf845c58f2b3aab77ac11a
thia9uera <thia9uera@gmail.com>
2017-04-01 12:29:06 +0100

    Adicionei as check marks "V" no balão da direita e uma funçãozinha pra colori-los

--- Unity Project/Assets/Scripts/BubbleView.cs
+++ Unity Project/Assets/Scripts/BubbleView.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 
 public class BubbleView : MonoBehaviour
 {
+    public Image[] m_CheckMarks;
+
+    public Color m_Green;
+    public Color m_Blue;
 
     public void SetText(string p_message)
     {
         Text messageText = transform.GetComponentInChildren<Text>();
 
         messageText.text = p_message;
+    }
+
+    public void ColorizeCheckMark(int p_index, Color p_color)
+    {
+        m_CheckMarks[p_index].color = p_color;
     }
 }
